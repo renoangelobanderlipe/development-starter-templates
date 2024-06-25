022c918c998965906abefcfaeae26ed0f159b179
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2024-06-25 17:29:22 +0800

    added path resolvers

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -8,4 +9,17 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+            '@pages': path.resolve(__dirname, './resources/js/pages'),
+            '@components': path.resolve(__dirname, './resources/js/components'),
+            '@assets': path.resolve(__dirname, './resources/js/assets'),
+            '@hooks': path.resolve(__dirname, './resources/js/hooks'),
+            '@routes': path.resolve(__dirname, './resources/js/routes'),
+            '@services': path.resolve(__dirname, './resources/js/services'),
+            '@utils': path.resolve(__dirname, './resources/js/utils'),
+
+        },
+    },
 });
